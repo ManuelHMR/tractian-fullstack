dc69d32555077760dca47cce6d2a7a2ee2f08ae8
ManuelHMR <manuelhmrjf@gmail.com>
2022-10-11 17:51:17 -0300

    fix: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,29 +8,10 @@ import routers from "./routers/routers";
 import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
+
 app.use(cors());
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  });
 app.use(express.json());
-
 app.use(routers);
-// if (process.env.NODE_ENV === "TEST"){
-//     app.use("/tests", testRouter);
-//   } 
 app.use(errorHandler);
 
 
